02b32fdec20a707f750f46dc1092fe528ecb15a9
RobinS-S <robins-s@wsad.com>
2022-09-08 01:00:48 +0200

    [ADD] Initial commit with empty base project including deps

---
+++ api/src/test/java/nl/bss/carrentapi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bss.carrentapi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
